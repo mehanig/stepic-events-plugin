6463a56416cbef83273e7f7200e963cb90255c91
mehanig <mehanig@gmail.com>
2015-10-10 03:30:39 +0300

    deface for reg form. no requests handled

---
+++ stepic_event.js
@@ -0,0 +1,22 @@
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.type = 'text/css';
+style.href = chrome.extension.getURL('style.css');
+(document.head||document.documentElement).appendChild(style);
+
+if (document.title.indexOf("Stepic") != -1) {
+    deface();
+}
+
+function deface(){
+    document.getElementById('id_password1').parentNode.remove();
+    document.getElementById('id_password2').parentNode.remove();
+    document.getElementById('id_email').parentNode.remove();
+    document.getElementsByClassName('reg-form__right')[0].remove();
+    document.getElementsByClassName('reg-form__title')[0].innerText = "Welcome to SECR.\nLet's play.";
+    fields = document.getElementsByTagName('label');
+    for (var i in fields) {
+        if (fields[i].innerText == 'Last name') { fields[i].innerText = "Email to contact"; }
+    }
+    document.getElementsByClassName('reg-form__btn')[0].innerText = "Start Challenges!";
+}
