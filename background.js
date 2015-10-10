b694acd774aef14d696566d7fe5c78f6ba7b5b85
mehanig <mehanig@gmail.com>
2015-10-11 00:41:53 +0300

    Working version! no time limit

--- background.js
+++ background.js
@@ -1,10 +1,28 @@
-//Runs in Chrome background completeley.
-// persistent set to TRUE? maybe shouldn't it?
+// persistent set to TRUE in manifest? maybe shouldn't it?
+
+'use strict;'
+var url_redirect = {
+    from: 'https://stepic.org/accounts/signup/',
+    to:   'https://stepic.org/accounts/signup/?next=/lesson/15767/'
+};
+
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+                    "from a content script:" + sender.tab.url :
+                    "from the extension");
+        if (request.greeting == "hello") {
+            sendResponse({farewell: "goodbye"});
+        }
+        if (request.greeting == "registration") {
+            sendResponse({farewell: "start"})
+        }
+});
+
+//Listener for redirecting to proper page after registration
+chrome.extension.onRequest.addListener(function(request, sender) {
+    if (sender.tab.url.startsWith(url_redirect.from) &&
+        sender.tab.url != url_redirect.to) {
+            chrome.tabs.update(sender.tab.id, {url: url_redirect.to});
+        }
+});
--- stepic_event.js
+++ stepic_event.js
@@ -1,3 +1,5 @@
+'use strict;'
+
 var style = document.createElement('link');
 style.rel = 'stylesheet';
 style.type = 'text/css';
@@ -8,13 +10,12 @@ if (document.title.indexOf("Stepic") != -1) {
     deface();
     replaceRegButton();
 }
+//Bzzz to background and test if url should be redirected
+chrome.extension.sendRequest({checker: "bzzz"});
 
 function deface(){
-    // document.getElementById('id_password1').parentNode.remove();
-    // document.getElementById('id_password2').parentNode.remove();
-    // document.getElementById('id_email').parentNode.remove();
     document.getElementsByClassName('reg-form__right')[0].remove();
-    document.getElementsByClassName('reg-form__title')[0].innerText = "Welcome to SECR.\nLet's play.";
+    document.getElementsByClassName('reg-form__title')[0].innerText = "Welcome to CEE SECR.\nLet's play.";
     fields = document.getElementsByTagName('label');
     for (var i in fields) {
         if (fields[i].innerText == 'Last name') { fields[i].innerText = "Email to contact"; }
@@ -25,11 +26,20 @@ function deface(){
 function hacked_registration(){
     username = document.getElementById('id_first_name').value;
     //Field says: enter Email but that's okay, hacked here; email -> surname;
-    //hash_email_generator() -> email;
+    //email_generator() -> email;
     usersurname = document.getElementById('id_last_name').value;
-    email = document.getElementById('id_email').value = 'ABC123sfasf@gmail.com';
-    pass = document.getElementById('id_password1').value = '123456';
-    pass2 = document.getElementById('id_password2').value = '123456';
+    email = document.getElementById('id_email').value = email_generator(username);
+    rand_password = getRandomInt(100000,9999999999999);
+    pass = document.getElementById('id_password1').value = rand_password;
+    pass2 = document.getElementById('id_password2').value = rand_password;
+}
+
+function email_generator(name) {
+    return name + (new Date).getTime() + '@test.com';
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function replaceRegButton(){
